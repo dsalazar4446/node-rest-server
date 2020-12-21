100c95ced4c45d391f7402a668757cddc4c864d9
Daniel Salazar <dsalazar446@gmail.com>
2020-12-21 09:27:27 -0400

    config env

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ process.env.PORT = process.env.PORT || 3000;
  * MONGO
  ********************************/
 const mongodb = {
-    url: process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://dsalazar446:CSR2rxBV8IyBAlZ3@cluster0.dkkto.mongodb.net/cafe'
+    url: process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.NODE_ENV
 }
 
-process.env.MONGO_URL_DB = mongodb.url;
+process.env.MONGO_URL = mongodb.url;
